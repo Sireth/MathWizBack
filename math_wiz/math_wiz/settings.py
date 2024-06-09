fa0af7a6501b1bd92571defc3abc5a2dc616eded
Sireth <mltya.usov04@gmail.com>
2024-06-09 06:52:53 +0300

    Project: static is defined

--- math_wiz/math_wiz/settings.py
+++ math_wiz/math_wiz/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -37,6 +37,21 @@ CORS_ALLOWED_ORIGINS = [
     'https://127.0.0.1',
     'https://0.0.0.0'
 ]
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+# Static files (CSS, JavaScript, Images)
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# Additional locations of static files
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
+# Media files (Uploaded files)
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Application definition
 
